6c011accb26d6dfa076b6a2d70aaac55ba3b00eb
abhi14patel <abhipatel052@gmail.com>
2020-11-21 14:49:24 +0530

    Version check api calling on login screen

--- app/src/main/java/com/serosoft/academia/Modules/Login/LoginActivity.kt
+++ app/src/main/java/com/serosoft/academia/Modules/Login/LoginActivity.kt
@@ -2,22 +2,41 @@ package com.serosoft.academia.Modules.Login
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.StringRequestListener
+import com.serosoft.academia.Interfaces.AsyncTaskCompleteListener
 import com.serosoft.academia.Modules.Dashboard.DashboardActivity
+import com.serosoft.academia.Networking.KEYS
 import com.serosoft.academia.R
 import com.serosoft.academia.Utils.BaseActivity
+import com.serosoft.academia.Utils.BaseURL
+import com.serosoft.academia.Utils.ConnectionDetector
 import com.serosoft.academia.Utils.ProjectUtils
+import org.w3c.dom.Element
+import org.xml.sax.InputSource
+import java.io.BufferedReader
+import java.io.StringReader
+import javax.xml.parsers.DocumentBuilderFactory
 
-class LoginActivity : BaseActivity() {
+class LoginActivity : BaseActivity(), AsyncTaskCompleteListener {
 
     var mContext: Context? = null
     var etUsername: EditText? = null
     var etPassword: EditText? = null
     var btnSignIn: Button? = null
     var btnForgotPassword: Button? = null
+    var tvInstanceVersion: TextView? = null
+    var tvAppVersion: TextView? = null
+    var tvInstanceURL: TextView? = null
+    var appVersion = ""
 
     val TAG: String? = LoginActivity::class.java.simpleName
 
@@ -29,6 +48,8 @@ class LoginActivity : BaseActivity() {
         mContext = this@LoginActivity
 
         Initialize()
+
+        populateContent()
     }
 
     private fun Initialize() {
@@ -37,9 +58,120 @@ class LoginActivity : BaseActivity() {
         etPassword = findViewById(R.id.etPassword);
         btnSignIn = findViewById(R.id.btnSignIn);
         btnForgotPassword = findViewById(R.id.btnForgotPassword);
+        tvAppVersion = findViewById(R.id.tvAppVersion)
+        tvInstanceURL = findViewById(R.id.tvInstanceURL)
+        tvInstanceVersion = findViewById(R.id.tvInstanceVersion)
 
         btnSignIn!!.setOnClickListener(this)
         btnForgotPassword!!.setOnClickListener(this)
+    }
+
+    private fun populateContent() {
+
+        if(ConnectionDetector.isConnectingToInternet(mContext!!)){
+
+            populateVersions()
+        }
+        else {
+            ProjectUtils.showLong(mContext, KEYS.NET_ERROR_MESSAGE)
+        }
+    }
+
+    private fun populateVersions(){
+
+        showProgressDialog(this)
+
+        //Here do api calling
+        val url: String = BaseURL.BASE_URL + KEYS.BUILD_INFO_METHOD
+
+        AndroidNetworking.get(url)
+            .setOkHttpClient(ProjectUtils.getUnsafeOkHttpClient())
+            .setPriority(Priority.HIGH)
+            .build()
+            .getAsString(object : StringRequestListener {
+                override fun onResponse(response: String) {
+                    // do anything with response
+                    //ProjectUtils.showLog(TAG, "Response"+response);
+                    hideProgressDialog()
+
+                    //Here parse Instance version
+                    /************** Read XML  */
+                    val br = BufferedReader(StringReader(response))
+                    val `is` = InputSource(br)
+                    /************  Parse XML  */
+                    try {
+                        val D = DocumentBuilderFactory.newInstance()
+                        val d = D.newDocumentBuilder()
+                        val mydocument = d.parse(`is`)
+
+                        // to get the elements from document 'mydocument'
+                        val E = mydocument.documentElement
+                        E.normalize()
+                        val MyListOfElemnts = mydocument.getElementsByTagName("buildInfo")
+
+                        for (i in 0 until MyListOfElemnts.length) {
+                            val N = MyListOfElemnts.item(i)
+                            val myElement = N as Element
+                            val N1 = myElement.getElementsByTagName("buildName").item(i).childNodes
+                            val node1 = N1.item(i)
+                            val name = node1.nodeValue
+                            val N2 =
+                                myElement.getElementsByTagName("buildVersion").item(i).childNodes
+                            val node2 = N2.item(i)
+                            val version = node2.nodeValue
+                            tvInstanceVersion!!.text = "$name $version | "
+                        }
+                    } catch (ex: Exception) {
+                        ex.printStackTrace()
+                        ProjectUtils.showLog(TAG, "" + ex.message)
+                    }
+                    appVersionFunction()
+                }
+
+                override fun onError(anError: ANError) {
+                    hideProgressDialog()
+                }
+            })
+    }
+
+    private fun appVersionFunction() {
+
+        //Here get app version
+        try {
+            val pInfo = packageManager.getPackageInfo(packageName, 0)
+            appVersion = pInfo.versionName
+            ProjectUtils.showLog(TAG, appVersion)
+            tvAppVersion!!.text = "V$appVersion | "
+
+        } catch (e: PackageManager.NameNotFoundException) {
+
+            e.printStackTrace()
+        }
+
+        //Here get instanceUrl
+        val url = BaseURL.BASE_URL
+        if (url.contains("://")) {
+
+            val separated = url.split("://").toTypedArray()
+            val str1 = separated[0]
+            val str2 = separated[1]
+            if (str2.contains(".academiaerp.com")) {
+
+                val separated1 = str2.split(".academiaerp.com").toTypedArray()
+                val st1 = separated1[0]
+                val st2 = separated1[1]
+                tvInstanceURL!!.text = st1
+            }
+            else {
+
+                val sttr2: String? = ProjectUtils.removeLastChar(str2)
+                tvInstanceURL!!.text = sttr2
+            }
+        }
+    }
+
+    override fun onTaskComplete(result: HashMap<String, String>?) {
+
     }
 
     override fun onClick(v: View?) {
---
+++ app/src/main/java/com/serosoft/academia/Utils/Flags.kt
@@ -0,0 +1,14 @@
+package com.serosoft.academia.Utils
+
+object Flags {
+
+    const val COLOR_FLAG = true
+    const val CHECK_VERSION = false
+    const val ENABLE_PAYTM_PRODUCTION = false
+    const val ENABLE_PARENTS_LOGIN = true
+    const val IS_APP_LIVE = false
+    const val ENABLE_MULTIPLE_ATTENDANCE_TYPES = true
+    const val DISEABLE_PERMISSION_FOR_TAIPEI = false                                                //True for CGC
+    const val IS_CLIENT_VELOCITY = false                                                            //True for velocity
+
+}
